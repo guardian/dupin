1149a5dc1b3c1fe4fa773cda0956a57b40e34e1c
Adam Fisher <adam.fisher@guardian.co.uk>
2017-06-02 17:39:59 +0100

    Add dependencies to setup.py
    requirements.txt isn't used for installs!

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(name="Dupin",
       description="Search for secrets in an Organisation's Github repositories",
       version='0.0.1',
       packages=['dupin', 'dupin.trufflehog'],
+      install_requires=[
+          "GitPython==2.1.1",
+          "PyGithub==1.34",
+          "pyyaml==3.12",
+          "PGPy==0.4.1"
+      ],
       entry_points={
           'console_scripts': [
               'dupin = dupin.dupin:main'
